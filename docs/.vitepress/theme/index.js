6bcb43acffb85dc4a9d7be3dea5ac36cd52b3b05
Denis <d9226059985@gmail.com>
2026-04-28 17:44:12 +0500

    Refactor sidebar navigation into SidebarBlock component

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,36 +5,29 @@ import './custom.css'
 
 const isOpen = ref(false)
 
+const SidebarBlock = () => h('div', { class: 'sidebar-custom-container' }, [
+  h('div', { 
+    class: ['sidebar-custom-trigger', { active: isOpen.value }],
+    onClick: (e) => {
+      e.stopPropagation();
+      isOpen.value = !isOpen.value;
+    }
+  }, [
+    h('span', { class: 'trigger-text' }, 'Общая информация'),
+    h('span', { class: 'vpi-chevron-right arrow-icon' })
+  ]),
+  isOpen.value ? h('div', { class: 'sidebar-custom-content' }, [
+    h('a', { href: '/Social-media-security-guide/about', class: 'custom-link' }, 'О проекте'),
+    h('a', { href: '/Social-media-security-guide/guide', class: 'custom-link' }, 'Инструкция по использованию')
+  ]) : null
+])
+
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'layout-top': () => h(ChecklistEnhancer),
-      'sidebar-nav-after': () => {
-        return h('div', { class: 'sidebar-custom-container' }, [
-          h('div', { 
-            class: ['sidebar-custom-trigger', { active: isOpen.value }],
-            onClick: (e) => {
-              e.stopPropagation();
-              isOpen.value = !isOpen.value;
-            }
-          }, [
-            h('span', { class: 'trigger-text' }, 'Общая информация'),
-            h('span', { class: 'vpi-chevron-right arrow-icon' })
-          ]),
-
-          isOpen.value ? h('div', { class: 'sidebar-custom-content' }, [
-            h('a', { 
-              href: '/Social-media-security-guide/about', 
-              class: 'custom-link' 
-            }, 'О проекте'),
-            h('a', { 
-              href: '/Social-media-security-guide/guide', 
-              class: 'custom-link' 
-            }, 'Инструкция по использованию')
-          ]) : null
-        ])
-      }
+      'sidebar-nav-after': () => h(SidebarBlock) 
     })
   }
 }
