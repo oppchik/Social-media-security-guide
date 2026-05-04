d3651dff858d2fc7ddfa11508e7c29e49731aeb6
Denis <d9226059985@gmail.com>
2026-05-04 23:35:13 +0500

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -12,37 +12,36 @@ export default {
     app.component('ChecklistEnhancer', ChecklistEnhancer)
     app.component('SocialGrid', SocialGrid)
 
-    provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
-      const isDark = document.documentElement.classList.contains('dark');
+     provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
+      const isDark = app.config.globalProperties.$vitepress.isDark
 
-      if (!document.startViewTransition) {
-        document.documentElement.classList.toggle('dark');
-        return;
+      if (!document.startViewTransition || window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
+        isDark.value = !isDark.value
+        return
       }
 
       const clipPath = [
         `circle(0px at ${x}px ${y}px)`,
         `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`
-      ];
+      ]
 
       const transition = document.startViewTransition(async () => {
-        document.documentElement.classList.toggle('dark');
-        await nextTick();
-      });
+        isDark.value = !isDark.value
+        await nextTick()
+      })
 
       transition.ready.then(() => {
         document.documentElement.animate(
+          { clipPath: isDark.value ? clipPath.reverse() : clipPath },
           {
-            clipPath: !isDark ? clipPath : clipPath.reverse(),
-          },
-          {
-            duration: 400,
-            easing: 'ease-in',
-            pseudoElement: !isDark ? '::view-transition-new(root)' : '::view-transition-old(root)',
+            duration: 450,
+            easing: 'ease-out',
+            pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
           }
-        );
-      });
-    });
+        )
+      })
+    })
+  },
 
   Layout() {
     return h(DefaultTheme.Layout, null, {
