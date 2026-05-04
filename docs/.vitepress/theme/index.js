328e64bcfb6b31932780a1c0cd0bc9502e047b88
Denis <d9226059985@gmail.com>
2026-05-04 23:41:49 +0500

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -8,47 +8,48 @@ const isOpen = ref(false)
 
 export default {
   extends: DefaultTheme,
+  setup() {
+    provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
+      if (!document.startViewTransition) {
+        return
+      }
+
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${Math.hypot(
+          Math.max(x, innerWidth - x),
+          Math.max(y, innerHeight - y)
+        )}px at ${x}px ${y}px)`
+      ]
+
+      const transition = document.startViewTransition(async () => {
+        document.documentElement.classList.toggle('dark')
+        await nextTick()
+      })
+
+      transition.ready.then(() => {
+        const isDark = document.documentElement.classList.contains('dark')
+        document.documentElement.animate(
+          {
+            clipPath: isDark ? clipPath.reverse() : clipPath
+          },
+          {
+            duration: 500,
+            easing: 'ease-in-out',
+            pseudoElement: isDark
+              ? '::view-transition-old(root)'
+              : '::view-transition-new(root)'
+          }
+        )
+      })
+    })
+  },
+
   enhanceApp({ app }) {
     app.component('ChecklistEnhancer', ChecklistEnhancer)
     app.component('SocialGrid', SocialGrid)
-
-     app.mixin({
-      mounted() {
-        if (window.appearanceLogicLoaded) return
-        window.appearanceLogicLoaded = true
-
-        provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
-          const isDark = app.config.globalProperties.$vitepress.isDark
-
-          if (!document.startViewTransition) {
-            isDark.value = !isDark.value
-            return
-          }
-
-          const clipPath = [
-            `circle(0px at ${x}px ${y}px)`,
-            `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`
-          ]
-
-          const transition = document.startViewTransition(async () => {
-            isDark.value = !isDark.value
-            await nextTick()
-          })
-
-          transition.ready.then(() => {
-            document.documentElement.animate(
-              { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-              {
-                duration: 400,
-                easing: 'ease-in',
-                pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
-              }
-            )
-          })
-        })
-      }
-    })
   },
+
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'layout-top': () => h(ChecklistEnhancer),
@@ -58,14 +59,13 @@ export default {
           h('div', { 
             class: ['sidebar-custom-trigger', { active: isOpen.value }],
             onClick: (e) => {
-              e.stopPropagation();
-              isOpen.value = !isOpen.value;
+              e.stopPropagation()
+              isOpen.value = !isOpen.value
             }
           }, [
             h('span', { class: 'trigger-text' }, 'Общая информация'),
             h('span', { class: 'vpi-chevron-right arrow-icon' })
           ]),
-
           isOpen.value ? h('div', { class: 'sidebar-custom-content' }, [
             h('a', { href: '/Social-media-security-guide/about', class: 'custom-link' }, 'О проекте'),
             h('a', { href: '/Social-media-security-guide/guide', class: 'custom-link' }, 'Инструкция по использованию')
