569a4e8f061443140145d76dcb9ddfb225b8a6d3
Denis <d9226059985@gmail.com>
2026-05-04 23:37:31 +0500

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -12,37 +12,43 @@ export default {
     app.component('ChecklistEnhancer', ChecklistEnhancer)
     app.component('SocialGrid', SocialGrid)
 
-     provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
-      const isDark = app.config.globalProperties.$vitepress.isDark
+     app.mixin({
+      mounted() {
+        if (window.appearanceLogicLoaded) return
+        window.appearanceLogicLoaded = true
 
-      if (!document.startViewTransition || window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
-        isDark.value = !isDark.value
-        return
-      }
+        provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
+          const isDark = app.config.globalProperties.$vitepress.isDark
 
-      const clipPath = [
-        `circle(0px at ${x}px ${y}px)`,
-        `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`
-      ]
-
-      const transition = document.startViewTransition(async () => {
-        isDark.value = !isDark.value
-        await nextTick()
-      })
-
-      transition.ready.then(() => {
-        document.documentElement.animate(
-          { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-          {
-            duration: 450,
-            easing: 'ease-out',
-            pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+          if (!document.startViewTransition) {
+            isDark.value = !isDark.value
+            return
           }
-        )
-      })
+
+          const clipPath = [
+            `circle(0px at ${x}px ${y}px)`,
+            `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`
+          ]
+
+          const transition = document.startViewTransition(async () => {
+            isDark.value = !isDark.value
+            await nextTick()
+          })
+
+          transition.ready.then(() => {
+            document.documentElement.animate(
+              { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+              {
+                duration: 400,
+                easing: 'ease-in',
+                pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+              }
+            )
+          })
+        })
+      }
     })
   },
-
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'layout-top': () => h(ChecklistEnhancer),
