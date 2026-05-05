1778e68f3d19bbd38db4c6fb9f3b9ca74450be9d
Denis <d9226059985@gmail.com>
2026-05-05 16:30:21 +0500

    Refactor theme component by removing sidebar logic
    Removed sidebar navigation enhancement logic and related state management.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
-import { h, ref } from 'vue'
+import { h } from 'vue'
 import ChecklistEnhancer from './ChecklistEnhancer.vue'
 import SocialGrid from './SocialGrid.vue'
 import './custom.css'
-
-const isOpen = ref(false)
 
 export default {
   extends: DefaultTheme,
@@ -17,24 +15,6 @@ export default {
     return h(DefaultTheme.Layout, null, {
       'layout-top': () => h(ChecklistEnhancer),
       'home-hero-image': () => h(SocialGrid), 
-      'sidebar-nav-after': () => {
-        return h('div', { class: 'sidebar-custom-container' }, [
-          h('div', { 
-            class: ['sidebar-custom-trigger', { active: isOpen.value }],
-            onClick: (e) => {
-              e.stopPropagation()
-              isOpen.value = !isOpen.value
-            }
-          }, [
-            h('span', { class: 'trigger-text' }, 'Общая информация'),
-            h('span', { class: 'vpi-chevron-right arrow-icon' })
-          ]),
-          isOpen.value ? h('div', { class: 'sidebar-custom-content' }, [
-            h('a', { href: '/Social-media-security-guide/about', class: 'custom-link' }, 'О проекте'),
-            h('a', { href: '/Social-media-security-guide/guide', class: 'custom-link' }, 'Инструкция по использованию')
-          ]) : null
-        ])
-      }
     })
   }
 }
