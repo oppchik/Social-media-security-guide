4d3d58b2c4c53ca6bb940d1b035c53bee2218b32
Denis <d9226059985@gmail.com>
2026-05-10 17:38:54 +0500

    Enhance error handling in ask endpoint
    Added error handling in the ask endpoint and improved source document extraction.

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -2,26 +2,42 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from dotenv import load_dotenv 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 
+
+load_dotenv()
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-os.environ["GOOGLE_API_KEY"] = "ТВОЙ_КЛЮЧ"
+api_key = os.getenv("GOOGLE_API_KEY")
+
 VECTOR_DB_DIR = "./chroma_db"
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+if not os.path.exists(VECTOR_DB_DIR):
+    print("База данных не найдена! Сначала запусти 'python ingest.py'")
+
 vectorstore = Chroma(persist_directory=VECTOR_DB_DIR, embedding_function=embeddings)
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key=api_key, 
+    temperature=0.3
+)
 
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
@@ -34,14 +50,16 @@ class Question(BaseModel):
 
 @app.post("/ask")
 async def ask(question: Question):
-    result = qa_chain.invoke({"query": question.text})
-    
-    sources = list(set([doc.metadata.get('source', 'unknown') for doc in result['source_documents']]))
-    
-    return {
-        "answer": result['result'],
-        "sources": sources
-    }
+    try:
+        result = qa_chain.invoke({"query": question.text})
+        sources = list(set([os.path.basename(doc.metadata.get('source', 'unknown')) for doc in result['source_documents']]))
+
+        return {
+            "answer": result['result'],
+            "sources": sources
+        }
+    except Exception as e:
+        return {"answer": f"Произошла ошибка: {str(e)}", "sources": []}
 
 if __name__ == "__main__":
     import uvicorn
