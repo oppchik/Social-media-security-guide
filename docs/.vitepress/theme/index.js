141f4645b36013e4445c83eb44a2d13b6043d90c
Denis <d9226059985@gmail.com>
2026-05-15 18:04:34 +0500

    Clean up index.js by removing unused code
    Removed unused imports and zoom initialization logic.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import { h, onMounted, watch, nextTick } from 'vue'
-import { useRoute } from 'vitepress'
-import mediumZoom from 'medium-zoom'
+import { h } from 'vue'
 import ChecklistEnhancer from './ChecklistEnhancer.vue'
 import SocialGrid from './SocialGrid.vue'
 import './custom.css'
@@ -11,20 +9,6 @@ export default {
   enhanceApp({ app }) {
     app.component('ChecklistEnhancer', ChecklistEnhancer)
     app.component('SocialGrid', SocialGrid)
-  },
-
-  setup() {
-    const route = useRoute()
-    
-    const initZoom = () => {
-      mediumZoom('.flip-back img', {
-        background: 'rgba(0, 0, 0, 0.9)', 
-        margin: 20                        
-      })
-    }
-
-    onMounted(() => initZoom())
-    watch(() => route.path, () => nextTick(() => initZoom()))
   },
 
   Layout() {
